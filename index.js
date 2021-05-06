316b8e21e513c3b5dd09311fec6da17b9ee6448b
sperr7489 <71534899+sperr7489@users.noreply.github.com>
2021-05-06 09:15:09 +0900

    2021/05/06 09:15

---
+++ index.js
@@ -0,0 +1,10 @@
+const loginBtn = document.querySelector(".tutor-page__login-btn");
+
+function pageMove(event)
+{
+
+    console.log("버튼이클릭되었다.");
+    window.location.replace("main.html");
+}
+
+loginBtn.addEventListener("click",pageMove);
