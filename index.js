f2157ac7359438c23c4df9257fd494572f431eab
sperr7489 <71534899+sperr7489@users.noreply.github.com>
2021-05-06 14:24:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const loginBtn = document.querySelector(".login-page__login-btn");
 function pageMove(event)
 {
     console.log("버튼이클릭되었다.");
-    window.location.replace("main.html");
+    window.location.href= "main.html";
+    
 }
 
 loginBtn.addEventListener("click",pageMove);
