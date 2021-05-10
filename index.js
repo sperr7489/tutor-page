4f053253ea3ad8145a317e3e08d280ce9f5e335a
sperr7489 <sperr@ajou.ac.kr>
2021-05-10 17:17:41 +0900

    s

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const loginBtn = document.querySelector(".login-page__login-btn");
 
-function pageMove(event)
-{
+function pageMove(event) {
     console.log("버튼이클릭되었다.");
-<<<<<<< HEAD
-    window.location.href= "main.html";
-    
-=======
-    location.href="main.html";
->>>>>>> b076ada133904000d94d84278f1657e8751b32aa
+    window.location.href = "main.html";
+
+    location.href = "main.html";
 }
 
-loginBtn.addEventListener("click",pageMove);
+loginBtn.addEventListener("click", pageMove);
