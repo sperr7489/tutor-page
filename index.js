0ebfe5c0fb59e07f3b705b914f7f1c2215a5e056
sperr7489 <71534899+sperr7489@users.noreply.github.com>
2021-05-12 16:52:59 +0900

    reset

--- index.js
+++
@@ -1,9 +0,0 @@
-const loginBtn = document.querySelector(".login-page__login-btn");
-
-function pageMove(event)
-{
-    console.log("버튼이클릭되었다.");
-    location.href="main.html";
-}
-
-loginBtn.addEventListener("click",pageMove);
