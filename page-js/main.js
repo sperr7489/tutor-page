e596659235c9b95d6450d6abe9c34f04f186bdd0
sperr7489 <71534899+sperr7489@users.noreply.github.com>
2021-05-17 14:46:15 +0900

    dd

--- page-js/main.js
+++ page-js/main.js
@@ -5,14 +5,35 @@ console.log(menuChild);
 console.log(menuArr);
 menuArr.forEach(element =>{
 
-    element.addEventListener("click", hoverScrollMenu);
-
-    console.log(element);
+    element.addEventListener("click", clickScrollMenu);
 
 
 });
 
-function hoverScrollMenu(element){
-    console.log(element.getAttribute("src"));
+function clickScrollMenu(event){
 
+    var target = event.target;
+    var source = target.getAttribute('src');   
+    var sibling = target.nextElementSibling;
+    if(target.style.opacity == 0.2)
+    {
+        target.style.opacity = 1;
+        sibling.style.display = 'none';
+    }
+    else{
+        target.style.opacity = 0.2;
+        sibling.style.display = 'block';
+        target.style.brightness ='40%';
+
+
+
+    }
+
+
+
+
+
+
+
+//event는 말그대로 event가 되었을 때를 나타내는것이다. 
 }
