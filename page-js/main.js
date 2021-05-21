3139491e6a6dfb792e55d33a21e2a235a5f05f5e
sperr7489 <71534899+sperr7489@users.noreply.github.com>
2021-05-21 12:36:13 +0900

    dd

--- page-js/main.js
+++ page-js/main.js
@@ -1,39 +1,50 @@
 const scrollmenu = document.querySelector(".scrollmenu");
 const menuChild = scrollmenu.children;
 const menuArr = Array.from(menuChild);
+console.log(scrollmenu);
 console.log(menuChild);
 console.log(menuArr);
 menuArr.forEach(element =>{
 
-    element.addEventListener("click", clickScrollMenu);
-
-
+    element.addEventListener("mouseover", mouseoverElement);
+    element.addEventListener("mouseout",mouseoutElement);
 });
 
-function clickScrollMenu(event){
+const profile = document.querySelector(".profile");
+
+profile.addEventListener("mouseover",mouseoverElement);
+profile.addEventListener("mouseout",mouseoutElement);
+
+function mouseoverElement(event){
 
     var target = event.target;
     var source = target.getAttribute('src');   
     var sibling = target.nextElementSibling;
-    if(target.style.opacity == 0.2)
-    {
-        target.style.opacity = 1;
-        sibling.style.display = 'none';
-    }
-    else{
+
         target.style.opacity = 0.2;
         sibling.style.display = 'block';
         target.style.brightness ='40%';
+        var str = source.split('/')[2].split('.')[0];
 
-
-
-    }
-
-
-
-
-
-
+        console.log(str);
 
 //event는 말그대로 event가 되었을 때를 나타내는것이다. 
+}
+function mouseoutElement(event){
+    var target = event.target;
+    var source = target.getAttribute('src');
+    var sibling = target.nextElementSibling;
+
+    target.style.opacity = 1.0;
+    sibling.style.display = 'none';
+    target.style.brightness = '100%';
+}
+
+function pageMove(event){
+    //클릭시에 페이지를 이동시키는 것.  
+    var target = event.target;
+    var source =target.getAttribute('src');
+    var str = source.split('/')[2].split('.')[0];
+
+
 }
